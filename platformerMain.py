aa6bf37f20c81efe4cb71b4e8a0d59c96d5fc263
lioncatdev <sankhayan2002@gmail.com>
2020-11-30 17:55:28 +0530

    Tidied up the code

--- platformerMain.py
+++ platformerMain.py
@@ -11,16 +11,21 @@ playerSprite=pygame.image.load("player\\playertmpst.png")
 playerSprite=pygame.transform.scale2x(playerSprite)
 SKY=(134 , 219 , 251)
 playerPos=[100,50]
+
 movingRight=False
 movingLeft=False
 isJumping=False
 moveSpeed=3
-global jumpCount
+
 jumpCount=9
+
+#Variables init End
+
+#music player
 pygame.mixer.init()
 pygame.mixer.music.load("Songs\\rough_draft_for_Kittycat.mp3")
 pygame.mixer.music.play(-1)
-#Variables init End
+#music player End
 
 #Screen init
 screen = pygame.display.set_mode((WINDOWWIDTH , WINDOWHEIGHT))
@@ -32,20 +37,9 @@ pygame.display.set_caption("platformer")
 gameMap=pytmx.load_pygame("TilesetnMap\\GameMap.tmx")
 #Tilemap loader End
 
-#MainLoop
 
-while running:
-
-    isFalling=True
-    isCollidingLeft=False
-    isCollidingRight=False
-    isJumping=False
-    playerRect=playerSprite.get_rect()
-    playerRect.x=playerPos[0]
-    playerRect.y=playerPos[1]
-    screen.fill(SKY)  # Change background color to sky blue color
-    clock.tick(60)
-    # TileMap Renderer start
+#Functions
+def TileMapRenderer(isFalling,isCollidingRight,isCollidingLeft):
     for layer in gameMap.visible_layers:
         if isinstance(layer, pytmx.TiledTileLayer):
             for x, y, gid in layer:
@@ -66,14 +60,9 @@ while running:
                 if (obj.name == "Right walls"):
                     if pygame.Rect(obj.x, obj.y, obj.width, obj.height).colliderect(playerRect):
                         isCollidingRight = True
+    return isFalling,isCollidingRight,isCollidingLeft
 
-    # TileMap Renderer End
-    for event in pygame.event.get():#Getting the events
-        if event.type == pygame.QUIT:#Checking if user has clicked X button on the window or not
-            pygame.quit()
-            sys.exit()
-            running = False
-
+def InputManager(movingRight,movingLeft,isJumping):
     if event.type==pygame.KEYDOWN:#Check if any key is pressed do
 
         if event.key==pygame.K_RIGHT:
@@ -89,38 +78,72 @@ while running:
             movingRight=False
         if event.key==pygame.K_LEFT:
             movingLeft=False
+    return movingRight, movingLeft,isJumping
 
-    if movingRight and not isCollidingRight: #If the player is moving Right and not colliding on the right side
-        movingRight=True
-    if movingLeft and not isCollidingLeft:#If the player is moving Left and not colliding on the left side
-        movingLeft=True
 
-    if isCollidingRight:
-        movingRight=False
-    if isCollidingLeft:
-        movingLeft=False
+def Movement(playerPos,movingRight,movingLeft,isJumping):
+        global jumpCount
+        if movingRight and not isCollidingRight:  # If the player is moving Right and not colliding on the right side
+            movingRight = True
+        if movingLeft and not isCollidingLeft:  # If the player is moving Left and not colliding on the left side
+            movingLeft = True
 
-    if movingRight :
-        playerPos[0] += moveSpeed#Move right
+        if isCollidingRight:
+            movingRight = False
+        if isCollidingLeft:
+            movingLeft = False
 
-    if movingLeft :
-        playerPos[0]-= moveSpeed#move left
+        if movingRight:
+            playerPos[0] += moveSpeed  # Move right
 
-    if(isFalling):#Gravity of player
-        playerPos[1]+=3
+        if movingLeft:
+            playerPos[0] -= moveSpeed  # move left
 
-    if isJumping and not isCollidingRight and not isCollidingLeft:
-        if jumpCount>=-9:
-            neg=1
-            playerPos[1]-=jumpCount**2*0.1*neg
-            jumpCount-=1
-            if jumpCount<0:
-                neg=-1
-        else:
-            isJumping=False
-            jumpCount=9
+        if (isFalling):  # Gravity of player
+            playerPos[1] += 3
 
-    screen.blit(playerSprite,playerRect)#Load player sprite
+        if isJumping and not isCollidingRight and not isCollidingLeft:
+            if jumpCount >= -9:
+                neg = 1
+                playerPos[1] -= jumpCount ** 2 * 0.1 * neg
+                jumpCount -= 1
+                if jumpCount < 0:
+                    neg = -1
+            else:
+                isJumping = False
+                jumpCount = 9
+
+        return playerPos
+
+
+#Functions End
+
+#MainLoop
+while running:
+
+    isFalling=True
+    isCollidingLeft=False
+    isCollidingRight=False
+    isJumping=False
+    playerRect=playerSprite.get_rect()
+    playerRect.x=playerPos[0]
+    playerRect.y=playerPos[1]
+    screen.fill(SKY)  # Change background color to sky blue color
+    clock.tick(60)
+    for event in pygame.event.get():#Getting the events
+        if event.type == pygame.QUIT:#Checking if user has clicked X button on the window or not
+            pygame.quit()
+            sys.exit()
+            running = False
+    #FunctionCall area
+    isFalling, isCollidingRight, isCollidingLeft = TileMapRenderer(isFalling, isCollidingRight, isCollidingLeft)#Check collisions and draw tilemap
+
+    movingRight, movingLeft, isJumping = InputManager(movingRight, movingLeft, isJumping)#Handle Input
+
+    playerPos=Movement(playerPos, movingRight, movingLeft, isJumping)#Set player movement
+    # FunctionCall area End
+
+    screen.blit(playerSprite,playerRect)#Draw player sprite
     pygame.display.update()
     pygame.display.flip()#Makes the window visible
 #MainLoop End
